14f44703bf8f27a6f6f9f0108a9812d2c23ea68c
kozincov <kozincov1998@gmail.com>
2021-04-26 23:25:35 +0300

    add project

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () => {
+    return <header className='header'>
+        <img src='https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg ' />
+    </header>;
+
+}
+
+export default Header;
